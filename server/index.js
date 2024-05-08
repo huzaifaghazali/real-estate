712d990ba4dbca7176739b3bd492b749778c6add
huzaifaghazali <huzaifaghazali@gmail.com>
2024-05-08 21:09:10 +0500

    create sign up api route

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { mongoConnect } from './db/mongo.js';
 import userRouter from './routes/userRoute.js';
+import authRouter from './routes/authRoute.js';
 
 const PORT = process.env.PORT || 3000;
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
 async function startServer() {
   await mongoConnect();
