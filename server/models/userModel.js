e979ccabd76d981a73632d6a0943ccebf2460ce3
huzaifaghazali <huzaifaghazali@gmail.com>
2024-05-10 16:53:38 +0500

    add google oauth functionality

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -18,6 +18,8 @@ const userSchema = new Schema(
     },
     avatar: {
       type: String,
+      default:
+        'https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_1280.png',
     },
   },
   { timestamps: true }
