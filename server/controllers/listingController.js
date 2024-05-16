d4e465a656e9621cfc83fe600429bbe1563385ce
huzaifaghazali <huzaifaghazali@gmail.com>
2024-05-16 14:42:01 +0500

    complete update listing functionality

--- server/controllers/listingController.js
+++ server/controllers/listingController.js
@@ -32,11 +32,11 @@ export const deleteListing = async (req, res, next) => {
 export const updateListing = async (req, res, next) => {
   const listing = await Listing.findById(req.params.id);
 
-  if(!listing) {
+  if (!listing) {
     return next(errorHandler(404, 'Listing not found!'));
   }
 
-  if(req.user.id !== listing.userRef) {
+  if (req.user.id !== listing.userRef) {
     return next(errorHandler(401, 'You can only update your own listings!'));
   }
 
@@ -44,11 +44,22 @@ export const updateListing = async (req, res, next) => {
     const updateListing = await Listing.findByIdAndUpdate(
       req.params.id,
       req.body,
-      {new: true }
+      { new: true }
     );
     res.status(200).json(updateListing);
   } catch (error) {
     next(error);
   }
-  
-}
+};
+
+export const getListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, 'Listing not found!'));
+    }
+    res.status(200).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
--- server/routes/listingRoute.js
+++ server/routes/listingRoute.js
@@ -4,6 +4,7 @@ import { verifyToken } from '../utils/verifyUser.js';
 import {
   createListing,
   deleteListing,
+  getListing,
   updateListing,
 } from '../controllers/listingController.js';
 
@@ -12,5 +13,6 @@ const router = express.Router();
 router.post('/create', verifyToken, createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
 router.post('/update/:id', verifyToken, updateListing);
+router.get('/get/:id', getListing);
 
 export default router;
